5e9572edf0b2266b82437c7aa3ed4b1f909ccf3b
prathamsoni002 <114599961+prathamsoni002@users.noreply.github.com>
2023-01-11 20:12:05 +0530

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,31 @@
+# level 3
+# ls = [10, 20, 30, 300, 800, 60, 700]
+# for i in ls:
+#     if i > 400:
+#         print("This is my purchase cart item")
+#         continue
+#     print(i)
+    # 10
+    # 20
+    # 30
+    # 300
+    # This is my purchase cart item
+    # 60
+    # This is my purchase cart item
+
+# level 2
+# name = input("Enter name ")
+# i = 0
+# for x in name:
+#     if x == "n":
+#         print("\nThe character 'n' present at index no ,", i, "value = ", x)
+#         continue
+#     #     yaha se he continue if x == "n"
+#     print(x, end=" ")
+#     i += 1
+
+# level 1
+# for i in range(1, 10):
+#     if i == 5:
+#         continue
+#     print(i)
